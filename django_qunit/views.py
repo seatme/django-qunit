238124b8e5883c7f3f3a26d5a87d9db35647eeb0
Matt Behrens <askedrelic@gmail.com>
2014-09-16 09:55:17 -0700

    woops, don't overwrite variables named json

--- django_qunit/views.py
+++ django_qunit/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 
 try:
-    import simplejson as json
+    import simplejson
 except ImportError:
-    import json
+    import json as simplejson
 
 import os
 
@@ -29,7 +29,7 @@ def get_suite_context(request, path):
     if 'suite.json' in files:
         file = open(os.path.join(full_path, 'suite.json'), 'r')
         json = file.read()
-        suite.update(json.loads(json))
+        suite.update(simplejson.loads(json))
 
     previous_directory = parent_directory(path)
 
